8b512fc493de19dd61208fb1aea2ff42c6fcd017
Eric Schrijver <eric@ericschrijver.nl>
2012-12-11 12:09:41 +0100

    Now it saves the moves to the database and synchs multiplayer !

--- bouge.js
+++ bouge.js
@@ -1,6 +1,8 @@
 Positions = new Meteor.Collection("positions");
 
 if (Meteor.is_client) {
+  Template.movable.preserve(["img"]);
+
   Template.hello.movables = function () {
     return Positions.find();
   };
@@ -23,8 +25,11 @@ if (Meteor.is_client) {
             console.log($("img").length);
             $("img").draggable( {
                 stop: function(ev,ui){
-                    console.log(Positions.fin);
-                    console.log("position:" + ui.position.left + "," + ui.position.top); 
+                    // How could we insert 2 fields in 1 update?
+                    Positions.update($(this).attr("id"), {$set: {top: ui.position.top }});
+                    Positions.update($(this).attr("id"), {$set: {left: ui.position.left }});
+                    console.log($(this).attr("id"));
+                    console.log("position:" + ui.position.left + "," + ui.position.top);
                 }
             } );
         };
@@ -36,32 +41,32 @@ if (Meteor.isServer) {
     if (Positions.find().count() === 0) {
       var positions = [{
                         "name": '0.svg',
-                        "top": 790,
-                        "left": 750},
+                        "top": 0,
+                        "left": 0},
                     {
                         "name": '1.svg',
-                        "top": 390,
-                        "left": -172},
+                        "top": 0,
+                        "left": 100},
                     {
                         "name": '2.svg',
-                        "top": 180,
-                        "left": 850},
+                        "top": 0,
+                        "left": 200},
                     {
                         "name": '3.svg',
                         "top": 150,
-                        "left": 970},
+                        "left": 0},
                     {
                         "name": '4.svg',
-                        "top": 30,
-                        "left": 150},
+                        "top": 150,
+                        "left": 300},
                     {
                         "name": '5.svg',
-                        "top": 100,
-                        "left": 500},
+                        "top": 150,
+                        "left": 450},
                     {
                         "name": '6.svg',
-                        "top": 706,
-                        "left": 226.5},
+                        "top": 300,
+                        "left": 0},
                     ];
       for (var i = 0; i < positions.length; i++)
         Positions.insert(positions[i]);
